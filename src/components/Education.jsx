c2958ffa6956f0efb9128be80cb7362756ccc01c
Kaushal Thakur <kthakur0578@gmail.com>
2026-07-04 15:12:51 +0530

    Update Responsive

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -3,7 +3,7 @@ const Education = () => (
     <div className="wrap">
       <div className="sec-hd sr" data-n="05">
         <span className="sec-hd-icon" aria-hidden="true">
-          <svg viewBox="1 4 22 16" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21.42 10.922a1 1 0 0 0-.019-1.838L12.83 5.18a2 2 0 0 0-1.66 0L2.6 9.08a1 1 0 0 0 0 1.832l8.57 3.908a2 2 0 0 0 1.66 0z"/><path d="M22 10v6"/><path d="M6 12.5V16a6 3 0 0 0 12 0v-3.5"/></svg>
+          <svg viewBox="1 1 22 22" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21.42 10.922a1 1 0 0 0-.019-1.838L12.83 5.18a2 2 0 0 0-1.66 0L2.6 9.08a1 1 0 0 0 0 1.832l8.57 3.908a2 2 0 0 0 1.66 0z"/><path d="M22 10v6"/><path d="M6 12.5V16a6 3 0 0 0 12 0v-3.5"/></svg>
         </span>
         <h2>Education<em>.</em></h2>
       </div>
